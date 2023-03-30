b1c2dc3dc3afc7f5c084fd91c8a3e64894481a50
ushiu1230 <ushiu1230@gmail.com>
2023-03-30 18:17:25 +0700

    Update 30/03/2023

--- CarlaScenario1.py
+++ CarlaScenario1.py
@@ -17,8 +17,10 @@ import random
 import time
 import numpy as np
 
-def main(): 
 
+def main(): 
+    #----------------------------------------------------------------------------------------------------------
+    # SET UP CONNECTION 
     # Connect to the client and retrieve the world object
     client = carla.Client('192.168.1.9', 2000)
     world = client.get_world()
@@ -40,13 +42,22 @@ def main():
     # We will aslo set up the spectator so we can see what we do
     spectator = world.get_spectator()
 
+
+
+    #----------------------------------------------------------------------------------------------------------
+    # GET SOME SPAWN POINT ON MAP AND DEFINE THE AUTOPILOT ROUTE
+
     spawn_points = world.get_map().get_spawn_points()
+
     # Draw the spawn point locations as numbers in the map
-    # for i, spawn_point in enumerate(spawn_points):
-    #     world.debug.draw_string(spawn_point.location, str(i), life_time=10)
+    for i, spawn_point in enumerate(spawn_points):
+        world.debug.draw_string(spawn_point.location, str(i), life_time=10)
 
     # Route 1
-    spawn_point_1 =  spawn_points[10]
+    spawn_point_1 =  spawn_points[60]
+    spawn_point_2 =  spawn_points[10]
+    spawn_point_3 =  spawn_points[47]
+
     # Create route 1 from the chosen spawn points
     route_1_indices = [149, 21, 105, 52, 104, 140, 10]
     route_1 = []
@@ -60,38 +71,77 @@ def main():
     #     spawn_points[ind].location
     #     world.debug.draw_string(spawn_points[ind].location, str(ind), life_time=60, color=carla.Color(255,0,0))
 
+    #----------------------------------------------------------------------------------------------------------
+    # get blueprint
     bp_lib = world.get_blueprint_library()
     vehicle_bp = bp_lib.filter("model3")[0]
     npc_bp = bp_lib.filter("cooper_s")[0]
     print(vehicle_bp)
+    print(npc_bp)
     
+    # Spawn actor
     vehicle = world.try_spawn_actor(vehicle_bp, spawn_point_1)
+    npc1 = world.try_spawn_actor(npc_bp, spawn_point_2)
+    npc2 = world.try_spawn_actor(npc_bp, spawn_point_3)
+
+    # Set view on our Actor
+    transform = carla.Transform(vehicle.get_transform().transform(carla.Location(x=-4,z=2.5)),vehicle.get_transform().rotation)
+    spectator.set_transform(transform)
+
+    # Set route for auto pilot
     traffic_manager.set_path(vehicle, route_1)
     
+
+    # Set maximum speed 
     traffic_manager.global_percentage_speed_difference(75)
-    # Set parameters of TM vehicle control, we don't want lane changes
+
+    # Ignore_Lights
     traffic_manager.ignore_lights_percentage(vehicle, 100)
-    pilot_mode = False
-    count = 0
+    
+
+    vehicle.apply_physics_control(carla.VehiclePhysicsControl(max_rpm = 100.0, center_of_mass = carla.Vector3D(0.0, 0.0, 0.0), torque_curve=[[0,40],[100,40]]))
+
+
+    # Set Angle and Speed for car
+
+
+
+    manual_mode = True
+    auto_mode = True
+    count = 300
+
+
+
+    #----------------------------------------------------------------------------------------------------------
+    # *******CONTROL******
     # In synchronous mode, we need to run the simulation to fly the spectator
     while True:
         world.tick()
-        
-        if (count < 500):
-            pilot_mode = True
-            count +=1
-        else: 
-            pilot_mode = False
-            count -=1
 
-        vehicle.set_autopilot(pilot_mode) # Give TM control over vehicle
         #set viewpoint at main actor
         transform = carla.Transform(vehicle.get_transform().transform(carla.Location(x=-4,z=2.5)),vehicle.get_transform().rotation)
         spectator.set_transform(transform)
+        if (manual_mode and count != 0):
+            print("tick: ", count, "speed: ", vehicle.get_acceleration())
+            if (count >= 100):
+                vehicle.apply_control(carla.VehicleControl(throttle=0.15, steer=0))
+                count = count - 1
+            elif (count >= 50 and count < 100):
+                vehicle.apply_control(carla.VehicleControl(throttle=0.15, steer=-0.3))
+                count = count - 1
+            elif (count > 0 and count < 50):
+                vehicle.apply_control(carla.VehicleControl(throttle=0, steer=0))
+                count = count - 1
+        else: 
+            manual_mode = False
+            vehicle.apply_control(carla.VehicleControl(throttle=0, steer=0))
+    
+        # vehicle.set_autopilot(auto_mode) # Give TM control over vehicle
        
         
 if __name__ == "__main__":
     try:
         main()
+        
     except KeyboardInterrupt:
         print(' - Exited by user.')
